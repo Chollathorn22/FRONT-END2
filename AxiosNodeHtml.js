c5d123cf6e16263dd542a91963c51c1ab05cf573
Chollathorn22 <s6506021622019@email.kmutnb.ac.th>
2024-02-05 10:55:19 +0700

    save

---
+++ AxiosNodeHtml.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const Sequelize = require('axios');
+const app = express();
+var bodyParser = require('body-parser');
+
+
+
+
+
+
+const base_url = "http:localhost:3000";
+
+
+app.set('view engine', 'ejs');
+app.set(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use(express.static(_dirname + '/public'));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(base_url + '/books');
+        res.render("books", {books: response.data});
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/book/:id", async(req, res) =>{
+    try {
+        const response = await axios.get(base_url + '/books/' + req.params.id);
+        res.render("books", {books: response.data});
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+app.post("/create", async(req, res) =>{
+    try {
+        const data ={title: req.body.title, author: req.body.author};
+        await axios.post(base_url + 'books', data);
+        res.redirect("/");
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/book/:id", async(req, res) =>{
+    try {
+        const response = await axios.get(
+            base_url + '/books/' + req.params.id);
+        res.render("update", {books: response.data});
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.post("/update/:id", async(req, res) =>{
+    try {
+        const data = { title: req.body.title, author: req.body.author };
+        await axios.put(base_url + '/books/' + req.params.id, data);
+        res.redirect("/");
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/delete/:id", async(req, res) =>{
+    try {
+        await axios.delete(base_url + '/books/' + req.params.id, id);
+        res.redirect("/");
+    } catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.listen(5500, () =>{
+    console.log('Server started on port 5500');
+});
+
+
+// npm init -y
