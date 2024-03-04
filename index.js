ff940ee20a72797b647edcb374405851f6f567a1
Chollathorn22 <s6506021622019@email.kmutnb.ac.th>
2024-03-04 09:54:24 +0700

    save

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://localhost:3000";
+const base_url = "http://10.104.7.55";
 
 app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine','ejs');
